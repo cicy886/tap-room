01e905ed5d1905ce18acc6e6387336a3387e7611
Sisi Vieira <cicy886@gmail.com>
2022-01-15 14:36:43 -0800

    Add the skeleton of KegControl component

---
+++ src/components/KegControl.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class KegControl extends React.Compoent {
+
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return(
+      <React.Fragment>
+
+      </React.Fragment>
+    )
+  }
+}
