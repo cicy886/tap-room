669d50969bea5ec074ca654f8a0de2beae4e873c
Sisi Vieira <cicy886@gmail.com>
2022-01-15 15:06:34 -0800

    Add state function so that it can toggle between list and form

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,17 +1,37 @@
 import React from "react";
+import NewKegForm from "./NewKegForm";
+import KegList from "./KegList";
 
 class KegControl extends React.Compoent {
 
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
   }
 
   render(){
+    let currentlyVisibleState = null;
+    let addTicketButton = null;
+    if (this.state.formVisibleOnpage){
+      currentlyVisibleState = <NewKegForm/>
+    } else {
+      currentlyVisibleState = <KegList/>
+    }
     return(
       <React.Fragment>
-
+        {currentlyVisibleState}
+        {addTicketButton}
       </React.Fragment>
     )
   }
 }
+
+export default KegControl;
