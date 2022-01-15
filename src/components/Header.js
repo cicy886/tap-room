c15dc39aedce4b2619d8188f195054ff3d5ae0eb
Sisi Vieira <cicy886@gmail.com>
2022-01-15 14:30:28 -0800

    Use taproom image on Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
+import taproomImage from "./../img/taproom.jpg";
 
-function Header(){
-  return(
-    <h1>Tap Room</h1>
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>Tap Room</h1>
+      <img src={taproomImage} alt="taproom" />
+    </React.Fragment>
   );
 }
 
