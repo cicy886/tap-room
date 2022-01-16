6a9609a3437488143f3e64a2e674cebd25232824
Sisi Vieira <cicy886@gmail.com>
2022-01-15 17:12:21 -0800

    Incorporate reusable form

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,29 +1,14 @@
 import React from "react";
 import {v4} from "uuid";
 import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
 
 function NewKegForm(props){
   return (
     <React.Fragment>
-      <form onSubmit={handleNewKegFormSubmission}>
-        <input
-        type="text"
-        name="name"
-        placeholder="Name"/>
-        <input
-        type="text"
-        name="brand"
-        placeholder="Brand"/>
-        <input
-        type="number"
-        name="price"
-        placeholder="Price"/>
-        <input
-        type="number"
-        name="alcoholContent"
-        placeholder="Alcohol Content Percentage"/>
-        <button type="submit">Sell!</button>
-      </form>
+      <ReusableForm
+        formSubmissionHandler={handleNewKegFormSubmission}
+        buttonText="Sell!"/>
     </React.Fragment>
   );
   function handleNewKegFormSubmission(event){
