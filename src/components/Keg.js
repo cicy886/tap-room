791b0155a5d77d49b6f00f939c14daf0e46ec138
Sisi Vieira <cicy886@gmail.com>
2022-01-15 16:44:24 -0800

    Wrap individual keg in a dive, add proptypes for id and function

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -4,11 +4,13 @@ import PropTypes from "prop-types";
 function Keg(props){
   return(
     <React.Fragment>
+      <div onClick = {()=> props.whenKegClicked(props.id)}>
       <h3>{props.name}</h3>
       <p>{props.brand}</p>
       <p>{props.price}</p>
       <p>{props.alcoholContent}</p>
       <hr/>
+      </div>
     </React.Fragment>
   );
 }
@@ -17,7 +19,9 @@ Keg.propTypes = {
   name: PropTypes.string.isRequired,
   brand: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
-  alcoholContent: PropTypes.number.isRequired
+  alcoholContent: PropTypes.number.isRequired,
+  id: PropTypes.string,
+  whenTicketClicked: PropTypes.func
 }
 
 export default Keg;
